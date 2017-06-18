9039cdb4130f0f8280a263224d913c3c41683e0c
davidtom <davidjtomczyk@gmail.com>
2017-06-18 13:32:35 -0400

    create word class. new instances have a random word for word attribute, taken from a library of words that is a class attribute

--- lib/word.rb
+++ lib/word.rb
@@ -1,3 +1,14 @@
 #<Description of class>
 class Word
+  attr_reader :word
+
+  @@library = ["potato", "unicorn", "quixotic", "sweater", "humidity", "mountain", "goldfish", "aquatic", "plebian", "zebra"]
+
+  def initialize()
+    @word = self.class.library.sample
+  end
+
+  def self.library
+    @@library
+  end
 end
