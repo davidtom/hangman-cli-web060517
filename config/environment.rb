1abbecf28e1a8a18ff3a1d671475611c65d7dd68
davidtom <davidjtomczyk@gmail.com>
2017-06-19 16:26:14 -0400

    create main_controller class to run all game options; create main_screen class to store and print game commands; updated run and environment accordingly

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,5 @@ Bundler.require
 require_relative "../lib/word.rb"
 require_relative "../lib/game_controller.rb"
 require_relative "../lib/gallows.rb"
+require_relative "../lib/main_screen.rb"
 require_relative "../lib/main_controller.rb"
